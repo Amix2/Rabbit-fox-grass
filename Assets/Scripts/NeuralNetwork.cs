c7fa8f48249ad5e47931d3e62809a789747e3e12
Konrad Radoń <konradradon@gmail.com>
2020-05-09 23:30:00 +0200

    Add saving network to file.

--- Assets/Scripts/NeuralNetwork.cs
+++ Assets/Scripts/NeuralNetwork.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Single;
 using MathNet.Numerics.Random;
@@ -60,7 +62,80 @@ public class NeuralNetwork : IBigBrain
         }
         return decision;
     }
+    
+    public static NeuralNetwork ReadFromFile(int fitness, String filePath)
+    {
+        //TODO implement reading from file
+        return new NeuralNetwork(Settings.Player.neuralNetworkLayers);
+    }
 
+    public void SaveToFile(int fitness, String filePath)
+    {
+        var stringBuilder = new StringBuilder(fitness.ToString()).AppendLine(",");
+
+        stringBuilder.Append(MatrixArrayToString(_weights));
+        
+        stringBuilder.AppendLine(",");
+        stringBuilder.Append(MatrixArrayToString(_biases));
+        
+        Debug.Log(stringBuilder.ToString());
+
+        System.IO.File.WriteAllText(filePath, stringBuilder.ToString());
+        //System.IO.File.WriteAllText(Environment.CurrentDirectory+"\\"+filePath, stringBuilder.ToString());
+    }
+
+    private static void CheckLayers(int[] layers)
+    {
+        if (layers.Length <= 1) throw new Exception($"Cannot create network for {layers.Length} layer");
+
+        foreach (var layer in layers)
+            if (layer <= 0)
+                throw new Exception($"Layer cannot have {layer} neurons");
+    }
+
+    private static void CheckWeights(int[] layers, Matrix<float>[] weights, Matrix<float>[] biases)
+    {
+        if (biases.Length != layers.Length - 1) throw new Exception("Wrong number of layers weights");
+        if (weights.Length != layers.Length - 1) throw new Exception("Wrong number of layers biases");
+
+        for (var i = 0; i < layers.Length - 1; i++)
+        {
+            if (weights[i].RowCount != layers[i + 1]) throw new Exception("Wrong number of neurons in layer weights");
+            if (weights[i].ColumnCount != layers[i])
+                throw new Exception("Wrong number of neurons from previous layer, layer size:"+layers[i + 1]+" actual num of columns in weights: "+weights[i].ColumnCount);
+            if (biases[i].RowCount!= layers[i + 1]) throw new Exception("Wrong number of neurons in layer biases");
+        }
+    }
+
+    private static void NormalizeVector(Matrix<float> input)
+    {
+        for (var i = 0; i < input.RowCount; i++)
+        {
+            input[i,0] = Sigmoid(input[i,0]);
+        }
+    }
+    
+    private static float Sigmoid(float value)
+    {
+        if (value >= 4f) return 1f;
+        float tmp = 1f - 0.25f * value;
+        tmp *= tmp;
+        tmp *= tmp;
+        tmp *= tmp;
+        tmp *= tmp;
+        return 1f / (1f + tmp);
+    }
+    
+    private Matrix<float> PreprocessInput(float[] input)
+    {
+        Matrix<float> preprocessed = DenseMatrix.Create(input.Length, 1, 0.0f);
+        for (var i = 0; i < input.Length; i++)
+        {
+            preprocessed[i, 0] = input[i];
+        }
+        return preprocessed;
+    }
+    
     private void GenerateRandomWeights()
     {
         var weights = new List<Matrix<float>>();
@@ -93,67 +168,53 @@ public class NeuralNetwork : IBigBrain
         _biases = biases.ToArray();
     }
 
-    private static void CheckLayers(int[] layers)
+    private static string MatrixArrayToString(Matrix<float>[] array)
     {
-        if (layers.Length <= 1) throw new Exception($"Cannot create network for {layers.Length} layer");
-
-        foreach (var layer in layers)
-            if (layer <= 0)
-                throw new Exception($"Layer cannot have {layer} neurons");
-    }
-
-    private static void CheckWeights(int[] layers, Matrix<float>[] weights, Matrix<float>[] biases)
-    {
-        if (biases.Length != layers.Length - 1) throw new Exception("Wrong number of layers weights");
-        if (weights.Length != layers.Length - 1) throw new Exception("Wrong number of layers biases");
-
-        for (var i = 0; i < layers.Length - 1; i++)
+        var stringBuilder= new StringBuilder();
+        stringBuilder.AppendLine("[");
+        
+        for (var i = 0; i < array.Length; i++)
         {
-            if (weights[i].RowCount != layers[i + 1]) throw new Exception("Wrong number of neurons in layer weights");
-            if (weights[i].ColumnCount != layers[i])
-                throw new Exception("Wrong number of neurons from previous layer, layer size:"+layers[i + 1]+" actual num of columns in weights: "+weights[i].ColumnCount);
-            if (biases[i].RowCount!= layers[i + 1]) throw new Exception("Wrong number of neurons in layer biases");
+            stringBuilder.Append(MatrixToString(array[i]));
+            if(i<array.Length-1) stringBuilder.AppendLine(",");
         }
+        stringBuilder.AppendLine();
+        stringBuilder.Append("]");
+
+        return stringBuilder.ToString();
     }
 
-    private static string ArrayToString<T>(T[] array)
+    private static string MatrixToString(Matrix<float> matrix)
     {
-        string ret = "";
-        foreach (var element in array)
+        var stringBuilder= new StringBuilder();
+        var hasMoreThanOneCol = matrix.ColumnCount > 1;
+        
+        stringBuilder.AppendLine("[");
+        for (var i = 0; i < matrix.RowCount; i++)
         {
-            ret += element + " ";
+            stringBuilder.Append(RowToString(matrix, i,hasMoreThanOneCol));
+            if(i< matrix.RowCount-1  && hasMoreThanOneCol)stringBuilder.AppendLine(",");
+            if(i< matrix.RowCount-1  && !hasMoreThanOneCol)stringBuilder.Append(",");
         }
-
-        return ret;
+        stringBuilder.AppendLine();
+        stringBuilder.Append("]");
+        
+        return stringBuilder.ToString();
     }
 
-    private Matrix<float> PreprocessInput(float[] input)
+    private static string RowToString(Matrix<float> matrix, int rowIndex, bool hasMoreThanOneCol)
     {
-        Matrix<float> preprocessed = DenseMatrix.Create(input.Length, 1, 0.0f);
-        for (var i = 0; i < input.Length; i++)
+        var stringBuilder= new StringBuilder();
+        
+        if(hasMoreThanOneCol)stringBuilder.Append("[");
+        for (var j = 0; j < matrix.ColumnCount; j++)
         {
-            preprocessed[i, 0] = input[i];
-        }
-        return preprocessed;
+            stringBuilder.Append(matrix[rowIndex, j].ToString(CultureInfo.InvariantCulture));
+            if( j < matrix.ColumnCount - 1)stringBuilder.Append(","); 
+        }            
+        if(hasMoreThanOneCol)stringBuilder.Append("]");
+
+        return stringBuilder.ToString();
     }
 
-    private static void NormalizeVector(Matrix<float> input)
-    {
-        for (var i = 0; i < input.RowCount; i++)
-        {
-            input[i,0] = Sigmoid(input[i,0]);
-        }
-    }
-    
-    private static float Sigmoid(float value)
-    {
-        if (value >= 4f) return 1f;
-        float tmp = 1f - 0.25f * value;
-        tmp *= tmp;
-        tmp *= tmp;
-        tmp *= tmp;
-        tmp *= tmp;
-        return 1f / (1f + tmp);
-    }
-    
 }
