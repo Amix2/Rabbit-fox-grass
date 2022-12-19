faf245fe064c986823d46426419ceab36a7379ed
Maciej Mucha <mmucha.00@gmail.com>
2022-12-19 21:57:31 +0100

    vr init

--- Assets/Scripts/Camera/CameraConroller.cs
+++ Assets/Scripts/Camera/CameraConroller.cs
@@ -25,38 +25,38 @@ public class CameraConroller : MonoBehaviour
 
     private void UpdatePosition()
     {
-        if (Input.GetMouseButton(1))
-        {
-            // move root based on mouse movement, camera is a child of root so it will move automatically
-            Vector3 mouseMove = new Vector3(Input.GetAxis("Mouse X"), 0f, Input.GetAxis("Mouse Y"));
-            Vector3 cameraRootMove = mouseMove * offsetLength * Settings.Player.cameraMoveSensitivity * Time.deltaTime;
-            if (cameraRootMove.sqrMagnitude > maxCameraFrameMove * maxCameraFrameMove) cameraRootMove = cameraRootMove.normalized * maxCameraFrameMove;
-            root.Translate(mouseMove * offsetLength * Settings.Player.cameraMoveSensitivity * Time.deltaTime);
-        }
+        //if (Input.GetMouseButton(1))
+        //{
+        //    // move root based on mouse movement, camera is a child of root so it will move automatically
+        //    Vector3 mouseMove = new Vector3(Input.GetAxis("Mouse X"), 0f, Input.GetAxis("Mouse Y"));
+        //    Vector3 cameraRootMove = mouseMove * offsetLength * Settings.Player.cameraMoveSensitivity * Time.deltaTime;
+        //    if (cameraRootMove.sqrMagnitude > maxCameraFrameMove * maxCameraFrameMove) cameraRootMove = cameraRootMove.normalized * maxCameraFrameMove;
+        //    root.Translate(mouseMove * offsetLength * Settings.Player.cameraMoveSensitivity * Time.deltaTime);
+        //}
     }
 
     private void UpdateDistance()
     {
         // canculate zoom based on % of distance to root, cameraScrollSensitivity and delta time
-        float change = offsetLength * Settings.Player.cameraScrollSensitivity * Input.mouseScrollDelta.y * Time.deltaTime;
-        float distance = (transform.position - root.position).magnitude;
-        if (change > distance - minCameraDistance)
-        {
-            // camera cant get closer than minCameraDistance to root
-            change = distance - minCameraDistance;
-        }
-        transform.Translate(Vector3.forward * change, Space.Self);
-        offsetLength -= change;
+        //float change = offsetLength * Settings.Player.cameraScrollSensitivity * Input.mouseScrollDelta.y * Time.deltaTime;
+        //float distance = (transform.position - root.position).magnitude;
+        //if (change > distance - minCameraDistance)
+        //{
+        //    // camera cant get closer than minCameraDistance to root
+        //    change = distance - minCameraDistance;
+        //}
+        //transform.Translate(Vector3.forward * change, Space.Self);
+        //offsetLength -= change;
     }
 
     private void UpdateAngle()
     {
-        if (Input.GetMouseButton(0))
-        {
-            // rotate root around Up vector to keep it flat on the ground
-            root.RotateAround(root.position, Vector3.up, Input.GetAxis("Mouse X") * Settings.Player.cameraRotateSensitivity * Time.deltaTime);
-            // change camera vertical angle
-            transform.RotateAround(root.position, root.right, Input.GetAxis("Mouse Y") * Settings.Player.cameraRotateSensitivity * Time.deltaTime);
-        }
+        //if (Input.GetMouseButton(0))
+        //{
+        //    // rotate root around Up vector to keep it flat on the ground
+        //    root.RotateAround(root.position, Vector3.up, Input.GetAxis("Mouse X") * Settings.Player.cameraRotateSensitivity * Time.deltaTime);
+        //    // change camera vertical angle
+        //    transform.RotateAround(root.position, root.right, Input.GetAxis("Mouse Y") * Settings.Player.cameraRotateSensitivity * Time.deltaTime);
+        //}
     }
 }
